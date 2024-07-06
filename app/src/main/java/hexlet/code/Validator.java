92a223df072e1356a384191bedbf3751916cfca4
Arlan <Arlan110022@Gmail.com>
2024-07-06 19:46:20 +0500

    minor changes

--- app/src/main/java/hexlet/code/Validator.java
+++ app/src/main/java/hexlet/code/Validator.java
@@ -5,14 +5,30 @@ import hexlet.code.schemas.NumberSchema;
 import hexlet.code.schemas.StringSchema;
 
 public class Validator {
+
+    /**
+     * Creates and returns a new instance of StringSchema for validating strings.
+     *
+     * @return StringSchema instance for string validation.
+     */
     public StringSchema string() {
         return new StringSchema();
     }
 
+    /**
+     * Creates and returns a new instance of NumberSchema for validating numbers.
+     *
+     * @return NumberSchema instance for number validation.
+     */
     public NumberSchema number() {
         return new NumberSchema();
     }
 
+    /**
+     * Creates and returns a new instance of MapSchema for validating maps.
+     *
+     * @return MapSchema instance for map validation.
+     */
     public MapSchema map() {
         return new MapSchema();
     }
