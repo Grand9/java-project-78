b13c4d052ea650967ecec4efa589038080e15a00
Arlan <Arlan110022@Gmail.com>
2024-07-06 12:39:17 +0500

    unused import delete

--- app/src/main/java/hexlet/code/Validator.java
+++ app/src/main/java/hexlet/code/Validator.java
@@ -4,8 +4,6 @@ import hexlet.code.schemas.MapSchema;
 import hexlet.code.schemas.NumberSchema;
 import hexlet.code.schemas.StringSchema;
 
-import java.util.Map;
-
 public class Validator {
     public StringSchema string() {
         return new StringSchema();
