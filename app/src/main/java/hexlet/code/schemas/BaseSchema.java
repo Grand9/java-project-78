b381419f6360bcef74f5d7846ff62e35cca07c0b
Arlan <Arlan110022@Gmail.com>
2024-07-06 19:47:30 +0500

    minor changes

--- app/src/main/java/hexlet/code/schemas/BaseSchema.java
+++ app/src/main/java/hexlet/code/schemas/BaseSchema.java
@@ -4,13 +4,34 @@ import java.util.HashMap;
 import java.util.Map;
 import java.util.function.Predicate;
 
+/**
+ * BaseSchema is an abstract class that provides a foundation for creating validation schemas.
+ * Extend this class to add custom validation checks or behaviors.
+ *
+ * @param <T> the type of value to be validated
+ */
 public abstract class BaseSchema<T> {
 
     private final Map<String, Predicate<T>> checks = new HashMap<>();
 
+    /**
+     * Adds a validation check with a given name and predicate.
+     * Override this method to customize validation checks.
+     *
+     * @param name      the name of the check
+     * @param predicate the predicate function for the check
+     */
     public void addCheck(String name, Predicate<T> predicate) {
         checks.put(name, predicate);
     }
+
+    /**
+     * Validates if the given value passes all registered checks.
+     * Override this method to customize validation logic.
+     *
+     * @param value the value to be validated
+     * @return true if the value is valid; false otherwise
+     */
 
     @SuppressWarnings("unchecked")
     public boolean isValid(Object value) {
